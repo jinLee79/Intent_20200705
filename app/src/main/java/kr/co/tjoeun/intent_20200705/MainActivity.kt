6892a8a4bc89e5743ac618140ed4db4ba79cd730
jslee <etoileettoi@gmail.com>
2020-07-05 12:42:12 +0900

    입력값을 Intent에 첨부.   => 받아서 사용해야 함

--- app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
@@ -10,12 +10,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        화면 전환(이동시키기)
+//        1. 첫번째 화면 버튼 누르면, 화면 전환(이동시키기)
         moveToFirstBtn.setOnClickListener {
 
 //            비행기 티켓 발권 (출발 / 도착)
             val myIntent =  Intent(this, FirstActivity::class.java)  //Kotlin은 new 키워드 없이 생성자 생성
 //            실제로 출발하기
+            startActivity(myIntent)
+
+        }
+
+//        2. 두번째 화면 버튼 누르면
+        sendToSecondBtn.setOnClickListener {
+
+//            입력한 메세지 받아오기
+            val inputMessage = messageEdt.text.toString()
+
+//            두번째 화면으로 이동하기
+            val myIntent = Intent(this, SecondActivity::class.java)
+            myIntent.putExtra("message", inputMessage)
             startActivity(myIntent)
 
         }
--- app/src/main/java/kr/co/tjoeun/intent_20200705/SecondActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200705/SecondActivity.kt
@@ -2,10 +2,12 @@ package kr.co.tjoeun.intent_20200705
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
     }
 }
