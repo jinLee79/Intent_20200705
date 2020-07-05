b83932d81cee7bd2721d50efdcb7675d9b8cf874
jslee <etoileettoi@gmail.com>
2020-07-05 12:47:01 +0900

    들고 오는 String 값을 텍스트뷰에 반영

--- app/src/main/java/kr/co/tjoeun/intent_20200705/SecondActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200705/SecondActivity.kt
@@ -9,5 +9,15 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+//        따로 이벤트가 있는게 아니라, 액티비티에 오자 마자 데이터 반영.
+
+//        intent: 이 화면에 진입할 때 사용한 Intent 객체
+//        getStringExtra : 우리가 첨부한 데이터가 String이니 이를 뽑아내자.
+//        "message" : 보내줄 떄 붙여준 이름표 (찾아낼 기준)
+        var receivedMessage = intent.getStringExtra("message")
+
+//        텍스트뷰의 text로 반영.
+        messageTxt.text = receivedMessage
+
     }
 }
