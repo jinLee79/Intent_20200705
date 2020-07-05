4f5d62d517b15fe693a43deea14da7d039375f73
jslee <etoileettoi@gmail.com>
2020-07-05 12:19:40 +0900

    첫번째 액티비티

--- app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200705/MainActivity.kt
@@ -1,11 +1,23 @@
 package kr.co.tjoeun.intent_20200705
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        화면 전환(이동시키기)
+        moveToFirstBtn.setOnClickListener {
+
+//            비행기 티켓 발권 (출발 / 도착)
+            val myIntent =  Intent(this, FirstActivity::class.java)  //Kotlin은 new 키워드 없이 생성자 생성
+//            실제로 출발하기
+            startActivity(myIntent)
+
+        }
     }
 }
