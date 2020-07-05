a48a0cdd3f0468d8cff849b19035db7142b949ce
jslee <etoileettoi@gmail.com>
2020-07-05 12:28:02 +0900

    기존 화면으로 돌아가기 (finish)

--- app/src/main/java/kr/co/tjoeun/intent_20200705/FirstActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200705/FirstActivity.kt
@@ -1,11 +1,21 @@
 package kr.co.tjoeun.intent_20200705
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        moveToBackBtn.setOnClickListener {
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+//            지금 보는 액티비티를 종료 => 이전 화면으로 돌아감
+            finish()
+        }
     }
 }
